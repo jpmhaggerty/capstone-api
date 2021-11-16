0862b1f41cd0f40ad25efbb2bf8d531f1f48b19e
Lindsay Farias <lindsayfarias@gmail.com>
2021-11-16 14:45:58 -0500

    installed knex

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 8080;
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello Express World!');
 });
 
-const server = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-})
-
-module.exports = server;
+module.exports = app;
