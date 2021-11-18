022139be40703e43ece881c7fdeb3626be6c6742
Brendan Kennedy <brendan.fj.kennedy@gmail.com>
2021-11-18 17:02:49 -0500

    added knex file

---
+++ knexfile.js
@@ -0,0 +1,45 @@
+// Update with your config settings.
+//to set up a container in docker for dvmnt: docker run --name container -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
+
+module.exports = {
+
+    development: {
+      client: 'pg',
+ 
+      connection: 'postgres://postgres:bacon@localhost/weatherappdb'
+    }, 
+  
+  
+    staging: {
+      client: 'postgresql',
+      connection: {
+        database: 'my_db',
+        user:     'username',
+        password: 'password'
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations'
+      }
+    },
+  
+    production: {
+      client: 'postgresql',
+      connection: {
+        database: 'my_db',
+        user:     'username',
+        password: 'password'
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations'
+      }
+    }
+  
+  };
