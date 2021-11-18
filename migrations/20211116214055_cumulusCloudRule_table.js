f0fe4e8aa35570249f92db8800d3d64a2016f927
Brendan Kennedy <brendan.fj.kennedy@gmail.com>
2021-11-18 14:49:24 -0500

    table seeding completed

---
+++ migrations/20211116214055_cumulusCloudRule_table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('cumulus_cloud_rule', table => {
+        table.increments('id');
+        table.string('constraint_name').notNullable();
+        table.integer('constraint_parameter_integer');
+        table.boolean('constraint_parameter_boolean');
+        table.integer('user_input_integer');
+        table.boolean('user_input_boolean')
+     })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cumulus_cloud_rule')
+  };
---
+++ seeds/debris_cloud_rule_seed.js
@@ -0,0 +1,107 @@
+exports.seed = function(knex) {
+    // Deletes ALL existing entries
+    return knex('debris_cloud_rule').truncate()
+      .then(function () {
+        // Inserts seed entries
+        return knex('debris_cloud_rule').insert([
+            //1
+          {   constraint_name: 'Have of the following occurred: ' , 
+              constraint_parameter_integer: null,
+              constraint_parameter_boolean: null,
+              user_input_integer: null,
+              user_input_boolean: null
+          },
+          //2
+          {   constraint_name: 'The debris cloud is observed to be detached from the parent cloud?' , 
+          constraint_parameter_integer: null ,
+          constraint_parameter_boolean: false,
+          user_input_integer:null ,
+          user_input_boolean: null
+      },
+      //3
+      {   constraint_name: 'The debris cloud is observed to have formed by the collapse of the parent cloud top to an altitude where the temperature is warmer than -10 °C?' , 
+              constraint_parameter_integer: null,
+              constraint_parameter_boolean: false,
+              user_input_integer: null,
+              user_input_boolean: null
+          },
+          //4
+          {   constraint_name: 'Any lightning discharges occuring within or from the debris cloud?' , 
+          constraint_parameter_integer: null,
+          constraint_parameter_boolean: false,
+          user_input_integer: null,
+          user_input_boolean: null
+      },
+      //5
+      {   constraint_name: 'Has it been 3 hours since any the occurrence of the above three conditions?' , 
+          constraint_parameter_integer: null,
+          constraint_parameter_boolean: true,
+          user_input_integer: null,
+          user_input_boolean: null
+      },
+      //6
+      {   constraint_name: 'Is the vehicle’s flight path through a debris cloud?' , 
+          constraint_parameter_integer: null,
+          constraint_parameter_boolean: false,
+          user_input_integer: null,
+          user_input_boolean: null
+      }, 
+      //7
+      {   constraint_name: 'Is the portion of the debris cloud at a slant distance of less than or equal to 5 nmi from the flight path is located entirely at altitudes where the temperature is colder than 0 °C?' , 
+      constraint_parameter_integer: null,
+      constraint_parameter_boolean: true,
+      user_input_integer: null,
+      user_input_boolean: null
+    },
+    //8
+    {   constraint_name: 'The MRR is less than +7.5 dBZ everywhere within the flight path?' , 
+      constraint_parameter_integer: null,
+      constraint_parameter_boolean: true,
+      user_input_integer: null,
+      user_input_boolean: null
+    },
+    //9
+    {   constraint_name: 'Is the vehicle’s flight path 0-3nmi away from the debris cloud?' , 
+      constraint_parameter_integer: null,
+      constraint_parameter_boolean: false,
+      user_input_integer: null,
+      user_input_boolean: null
+    },
+    //10
+    {   constraint_name: 'Is there is at least one working field mill at a horizontal distance of less than or equal to 5 nmi from the debris cloud?' , 
+      constraint_parameter_integer: null,
+      constraint_parameter_boolean: true,
+      user_input_integer: null,
+      user_input_boolean: null
+    },
+    //11
+    {   constraint_name: 'The absolute values of all electric field measurements at a horizontal distance of less than or equal to 5 nmi from the flight path, and at each field mill, have been less than 1000 V m-1 for at least 15 minutes?' , 
+      constraint_parameter_integer: null,
+      constraint_parameter_boolean: true,
+      user_input_integer: null,
+      user_input_boolean: null
+    },
+    //12
+    {   constraint_name: 'Is the largest radar reflectivity from any part of the debris cloud less than or equal to a slant distance of 5 nmi from the flight path has been less than +10 dBZ for at least 15 minutes?' , 
+      constraint_parameter_integer: null,
+      constraint_parameter_boolean: true,
+      user_input_integer: null,
+      user_input_boolean: null
+    },
+    //13
+    {   constraint_name: 'Is the portion of the debris cloud at a slant distance of less than or equal to 5 nmi from the flight path is located entirely at altitudes where the temperature is colder than 0 °C?' , 
+      constraint_parameter_integer: null,
+      constraint_parameter_boolean: true,
+      user_input_integer: null,
+      user_input_boolean: null
+    },
+    //14
+    {   constraint_name: 'Is the MRR is less than +7.5 dBZ at every point at a slant distance of less than or equal to 1 nmi from the flight path?' , 
+      constraint_parameter_integer: null,
+      constraint_parameter_boolean: true,
+      user_input_integer: null,
+      user_input_boolean: null
+    }
+        ]);
+      });
+  };
