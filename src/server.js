1641cd1176a91b8e2c0982ecafbd44bf8c2fb56a
James Haggerty <jpm.haggerty@gmail.com>
2021-12-01 10:53:04 -0500

    fixed connection string with PG vars

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-const app = require("./app");
-const port = 8080;
+require('dotenv').config();
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+const app = require("./app");
+const port = process.env.PGPORT;
+
+app.listen( port || 8080 )
