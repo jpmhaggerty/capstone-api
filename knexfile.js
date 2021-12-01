4d4e3b8a17ad04233c90b38bea82679cf5a53d88
James Haggerty <jpm.haggerty@gmail.com>
2021-12-01 10:48:01 -0500

    Preparing for pipeline

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,14 @@
 // Update with your config settings.
 //to set up a container in docker for dvmnt: docker run --name container -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
+//postgres://${process.env.PG_USER}:${process.env.APP_DB_ADMIN_PASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PG_DATABASE}
+require('dotenv').config();
+
+const connection = {
+  connectionString: `postgres://${process.env.PG_USER}:${process.env.APP_DB_ADMIN_PASSWORD}@${process.env.PGHOST}:${process.env.PG_DATABASE}`,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
 module.exports = {
 
@@ -27,19 +36,6 @@ module.exports = {
     },
 
     production: {
-      client: 'postgresql',
-      connection: {
-        database: 'my_db',
-        user:     'username',
-        password: 'password'
-      },
-      pool: {
-        min: 2,
-        max: 10
-      },
-      migrations: {
-        tableName: 'knex_migrations'
-      }
-    }
-
+      client: 'pg',
+      connection
   };
