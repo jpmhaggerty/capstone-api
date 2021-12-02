d0da1d4aca9cecd0eb9237d6c0823936b7bcba7a
James Haggerty <jpm.haggerty@gmail.com>
2021-12-01 23:30:36 -0500

    Fixed .env

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,18 @@
 // Update with your config settings.
 //to set up a container in docker for dvmnt: docker run --name container -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
 //postgres://${process.env.PG_USER}:${process.env.APP_DB_ADMIN_PASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PG_DATABASE}
-// require('dotenv').config();
+require('dotenv').config();
 
-// const connect = {
-//   connectionString: `postgres://${process.env.PG_USER}:${process.env.APP_DB_ADMIN_PASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PG_DATABASE}`,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// };
+const connect = {
+  client: "pg",
+  connection: `postgres://${process.env.PG_USER}:${process.env.APP_DB_ADMIN_PASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PG_DATABASE}`,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
 module.exports = {
-  development: {
-    client: "pg",
-    connection: "postgres://postgres:bacon@localhost:5432/weatherappdb",
-  },
-
-  staging: {
-    client: "pg",
-    connection: "postgres://postgres:bacon@localhost:5432/weatherappdb",
-  },
-
-  production: {
-    client: "pg",
-    connection: "postgres://postgres:bacon@localhost:5432/weatherappdb",
-  },
+  development: connect,
+  staging: connect,
+  production: connect,
 };
